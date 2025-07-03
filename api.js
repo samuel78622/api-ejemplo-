cf56f615569c732e4b194fb9ee3e2d18fb3dbc36
samuel78622 <samuelchaparro88@gmail.com>
2025-07-03 17:23:16 -0500

    first comit

--- api.js
+++ api.js
@@ -1,17 +1,37 @@
 async function getDivisa() {
   const res = await fetch("https://uy.dolarapi.com/v1/cotizaciones");
   const divisa = await res.json();
-modendalist = ""
-divisa.forEach(estado => {
-    modendalist+=
-    estado.moneda +" / "+ 
-    estado.nombre +" / "+ 
-    estado.compra +" / "+ 
-    estado.venta +" / "+ 
-    
-    estado.fechaActualizacion + "<br>"
-});
-console.log(modendalist);
-document.getElementById("emails").innerHTML= modendalist
-}
 
+  let modendalist = `
+    <table>
+      <thead>
+        <tr>
+          <th>Moneda</th>
+          <th>Nombre</th>
+          <th>Compra</th>
+          <th>Venta</th>
+          <th>Última Actualización</th>
+        </tr>
+      </thead>
+      <tbody>
+  `;
+
+  divisa.forEach(estado => {
+    modendalist += `
+      <tr>
+        <td>${estado.moneda}</td>
+        <td>${estado.nombre}</td>
+        <td>${estado.compra}</td>
+        <td>${estado.venta}</td>
+        <td>${estado.fechaActualizacion}</td>
+      </tr>
+    `;
+  });
+
+  modendalist += `
+      </tbody>
+    </table>
+  `;
+
+  document.getElementById("emails").innerHTML = modendalist;
+}
